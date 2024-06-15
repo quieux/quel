87c3cb6f1b6766c12e64390fa47f3ae792049cb2
Ezequiel Carvalho <ezequielalves32@gmail.com>
2024-06-15 14:56:09 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,25 +3,43 @@
 # Diretório de origem (assumido estar no mesmo diretório que o script)
 SOURCE_DIR="$(dirname "$0")/arquivos"
 
-# Diretório de destino (raiz do sistema)
-DEST_DIR="/"
-
 # Verifica se o diretório de origem existe
 if [ ! -d "$SOURCE_DIR" ]; then
   echo "O diretório de origem '$SOURCE_DIR' não existe."
   exit 1
 fi
 
-# Copia todo o conteúdo do diretório de origem para o diretório de destino
-cp -R "$SOURCE_DIR"/* "$DEST_DIR"
+# Função para copiar arquivos e diretórios preservando a estrutura
+copy_files() {
+  local src_dir=$1
+  local dest_dir=$2
+
+  # Copia todos os arquivos e diretórios, excluindo o próprio diretório fonte
+  find "$src_dir" -mindepth 1 -exec cp -r {} "$dest_dir" \;
+}
+
+# Copia todo o conteúdo do diretório bin de origem para /bin no destino
+copy_files "$SOURCE_DIR/bin" "/bin"
 
 # Verifica se os arquivos foram copiados corretamente
 if [ $? -ne 0 ]; then
-  echo "Erro ao copiar arquivos do diretório '$SOURCE_DIR' para '$DEST_DIR'."
+  echo "Erro ao copiar arquivos do diretório '$SOURCE_DIR/bin' para '/bin'."
   exit 1
 fi
 
-# Aplica permissões de execução aos arquivos bin/quel e bin/quel-get
+# Cria o diretório /etc/quel/ se ele não existir
+mkdir -p "/etc/quel"
+
+# Copia o arquivo repo para /etc/quel/
+cp "$SOURCE_DIR/etc/quel/repo" "/etc/quel/repo"
+
+# Verifica se o arquivo repo foi copiado corretamente
+if [ $? -ne 0 ]; then
+  echo "Erro ao copiar o arquivo repo para /etc/quel/."
+  exit 1
+fi
+
+# Aplica permissões de execução aos arquivos /bin/quel e /bin/quel-get
 chmod +x /bin/quel /bin/quel-get
 
 # Verifica se o chmod foi aplicado corretamente
